622571cf8fe3698ebd02b198ccb15014a71a9b5f
pg <pg@gmail.com>
2019-08-11 00:13:31 +0530

    Compiled with C++ and syscall.c removed.

--- Projects/HelloWorld/Src/main.c
+++ Projects/HelloWorld/Src/main.c
@@ -14,13 +14,28 @@ void SystemClock_Config(void);
 static void CPU_CACHE_Enable(void);
 static void Error_Handler(void);
 
-static uint8_t * HelloString = "Hello World ";
+static uint8_t * HelloString = (uint8_t *) "Hello World ";
 
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
 int main(void)
 {
   /* Enable the CPU Cache */
   CPU_CACHE_Enable();
 
+  /* STM32F7xx HAL library initialization:
+       - Configure the Flash prefetch
+       - Systick timer is configured by default as source of time base, but user 
+         can eventually implement his proper time base source (a general purpose 
+         timer for example or other time source), keeping in mind that Time base 
+         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+         handled in milliseconds basis.
+       - Set NVIC Group Priority to 4
+       - Low Level Initialization
+     */
   HAL_Init();
 
   /* Configure the system clock to 216 MHz */
@@ -150,3 +165,32 @@ static void Error_Handler(void)
   }
 }
 
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
